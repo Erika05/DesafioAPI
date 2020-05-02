9ae397e503438d71b187d0f0097298d92b9780b3
ERIKA MARIA MACHADO <eri.ka.maria@hotmail.com>
2020-05-01 23:19:18 -0300

    Adicionado teste autenticação OAuth2 api spotfy

---
+++ RestSharpTemplate/Requests/Spotify/GerarToken/GerarTokenRequests.cs
@@ -0,0 +1,27 @@
+﻿using DesafioAPI.Bases;
+using RestSharp;
+using System;
+using System.Text;
+
+namespace DesafioAPI.Requests.Spotify.PlayList
+{
+    public class GerarTokenRequests : RequestBase
+    {
+        public static string acessToken;
+
+        public GerarTokenRequests(string client_credentials)
+        {          
+            // httpBasicAuthenticator = true;
+            apiGerarTokenSpotfy = true;
+            method = Method.POST;
+            headers.Add("Authorization", "Basic " + client_credentials);
+            headers.Remove("Content-Type");
+        }
+
+        public void SetParameters(string refresh_token)
+        {
+            parametrosBody.Add("grant_type", "refresh_token");
+            parametrosBody.Add("refresh_token", refresh_token);
+        }
+    }
+}
