99001dfd4f50d487a70cb94f7ce85bcb93f14b48
ERIKA MARIA MACHADO <eri.ka.maria@hotmail.com>
2020-06-11 16:32:59 -0300

    Adicionado paralelização dos testes

--- RestSharpTemplate/Properties/AssemblyInfo.cs
+++ RestSharpTemplate/Properties/AssemblyInfo.cs
@@ -36,4 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 //Paralelização dos testes
-//[assembly: LevelOfParallelism(5)]
+[assembly: LevelOfParallelism(25)]
